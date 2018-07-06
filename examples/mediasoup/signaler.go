900e45f9bc3bd37d8db4c1dff0a9a5245813f479
John R. Bradley <jrb@turrettech.com>
2018-07-06 11:19:02 -0500

    WIP

---
+++ examples/mediasoup/signaler.go
@@ -0,0 +1,116 @@
+package mediasoup
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/pkg/errors"
+	"log"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Signaler struct {
+	url        string
+	conn       *websocket.Conn
+	done       chan struct{}
+	idMapMutex *sync.Mutex
+	idMap      map[int]chan []byte
+	rand       *rand.Rand
+}
+
+type request struct {
+	Request bool        `json:"request"`
+	ID      int         `json:"id"`
+	Method  string      `json:"method"`
+	Data    interface{} `json:"data"`
+}
+
+type response struct {
+	Response bool        `json:"response"`
+	ID       int         `json:"id"`
+	Ok       bool        `json:"ok"`
+	Data     interface{} `json:"data"`
+}
+
+func NewSignaler(rawUrl string) (*Signaler, error) {
+
+	header := make(http.Header)
+	header.Add("Sec-WebSocket-Protocol", "protoo")
+
+	c, _, err := websocket.DefaultDialer.Dial(rawUrl, header)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to connect to signaler")
+	}
+
+	rs := rand.NewSource(time.Now().UnixNano())
+
+	s := &Signaler{}
+	s.conn = c
+	s.idMapMutex = &sync.Mutex{}
+	s.rand = rand.New(rs)
+	s.done = make(chan struct{})
+	s.idMap = make(map[int]chan []byte)
+
+	go func() {
+		defer close(s.done)
+		for {
+			_, message, err := c.ReadMessage()
+			t := &response{}
+			err = json.Unmarshal(message, t)
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+
+			if v, ok := s.idMap[t.ID]; ok {
+				v <- message
+			} else {
+				log.Printf("unknown ID %v, recv: %s", t.ID, message)
+			}
+		}
+	}()
+
+	return s, nil
+}
+
+func (s *Signaler) Request(data interface{}) (chan []byte, error) {
+
+	var newId int
+	{
+		s.idMapMutex.Lock()
+		defer s.idMapMutex.Unlock()
+		// Create a random, unused id
+		ok := true
+		for ok {
+			newId = int(s.rand.Int31())
+			_, ok = s.idMap[newId]
+		}
+
+		s.idMap[newId] = make(chan []byte)
+	}
+
+	r := request{
+		Request: true,
+		Method:  "mediasoup-request",
+		ID:      newId,
+		Data:    data,
+	}
+
+	fmt.Printf("Sending request: %v\n", r)
+
+	err := s.conn.WriteJSON(r)
+
+	if err != nil {
+		delete(s.idMap, newId)
+		return nil, errors.Wrap(err, "Unable to send request")
+	}
+
+	return s.idMap[newId], nil
+}
+
+func (s *Signaler) Close() {
+	close(s.done)
+}
